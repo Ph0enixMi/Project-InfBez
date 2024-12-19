2284cfa6ae1a59049d0b131f3113df81d062c808
Ph0enixMi <mike.koshevnikov03@gmail.com>
2024-12-19 20:05:09 +0300

    Обновлена функция генерации шума, добавлена модель для аномального датасета

--- data_noise.py
+++ data_noise.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 
-
 df = pd.read_csv('Датасет + описание\diabetes_BRFSS2015.csv')
 
-
-def add_noise_random_values(column, noise_level=0.1, change_ratio=0.1):
+def add_noise_random_values(column, change_ratio=0.5):
+    max_value = column.max()
     n_changes = int(len(column) * change_ratio)
     indices_to_change = np.random.choice(column.index, size=n_changes, replace=False)
-    noise = np.random.normal(0, noise_level, size=n_changes)
-    column.loc[indices_to_change] = column.loc[indices_to_change] + noise
+    
+    random_values = np.random.randint(0, max_value + 1, size=n_changes)
+    column.loc[indices_to_change] = random_values
     return column
 
 for column in df.columns:
     if df[column].dtype in ['float64', 'int64']:
-        df[column] = add_noise_random_values(df[column], noise_level=0.1, change_ratio=0.2)
+        df[column] = add_noise_random_values(df[column], change_ratio=0.3)
 
-df.to_csv('Датасет с аномалиями\modified_dataset.csv', index=False)
+df.to_csv(r'Датасет с аномалиями\modified_dataset.csv', index=False)
